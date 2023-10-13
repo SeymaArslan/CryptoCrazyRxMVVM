ccd9af5646f3e3054c5f00d1fe232c91befe3d9f
SeymaArslan <seymarslannn@gmail.com>
2023-10-13 10:58:28 +0300

    Use binding

---
+++ CryptoCrazyRxMVVM/View/CryptoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by Seyma on 13.10.2023.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
